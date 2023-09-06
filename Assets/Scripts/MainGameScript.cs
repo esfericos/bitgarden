d9fde314c9d0a07249f872a0977876947db764d3
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2023-09-05 21:15:40 -0300

    Add GameScene and MainGameScript

---
+++ Assets/Scripts/MainGameScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGameScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Started MainGameScript");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
