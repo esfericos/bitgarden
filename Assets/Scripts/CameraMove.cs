595f307f2821665300c30e0a9133121354850277
Yuri <49888082+YuriRDev@users.noreply.github.com>
2023-10-01 09:57:18 -0300

    ```Camera``` - Camera movement - Zoom in/out & Scroll X,Y

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    /** Camera X,Y Movement variables*/
+    private Vector3 Origin;
+    private Vector3 Difference;
+    private Vector3 ResetCamera;
+    private bool drag = false;
+
+    /** Camera zoom control variables*/
+    public float ZoomChange;
+    public float SmoothChange;
+    public float MinSize, MaxSize;
+    private Camera cam;
+
+
+    private void Start()
+    {
+        ResetCamera = Camera.main.transform.position;
+        cam = GetComponent<Camera>();
+    }
+
+    /** Camera Zoom Control */
+    private void Update()
+    {
+        if(Input.mouseScrollDelta.y > 0)
+        {
+            cam.orthographicSize -= ZoomChange * Time.deltaTime * SmoothChange;
+        }
+
+        if(Input.mouseScrollDelta.y < 0)
+        {
+            cam.orthographicSize += ZoomChange * Time.deltaTime * SmoothChange;
+        }
+
+        cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, MinSize, MaxSize);
+    }
+
+    /** Camera X,Y Movement */
+    private void LateUpdate()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            Difference = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - Camera.main.transform.position);
+
+            if(drag == false)
+            {
+                drag = true;
+                Origin = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+        } else
+        {
+            drag = false;
+        }
+
+        if(drag)
+        {
+            Camera.main.transform.position = Origin - Difference;
+        }
+
+        if(Input.GetMouseButton(1))
+        {
+            Camera.main.transform.position = ResetCamera;
+        }
+
+    }
+}
