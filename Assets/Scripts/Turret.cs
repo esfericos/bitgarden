3bf2426c49c7573a8fff80121d0f9235c6e92d2e
Gabriel Ribeiro <gabrielribss29@gmail.com>
2023-10-05 18:01:08 -0300

    Fixing turret bow movement

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -44,9 +44,9 @@ public class Turret : MonoBehaviour
 
     private void RotateTowardsTarget()
     {
-        float angle = Mathf.Atan2(_target.position.y - transform.position.y, _target.position.x - transform.position.x) * Mathf.Rad2Deg - 90f;
+        float angle = Mathf.Atan2(_target.position.y - transform.position.y, _target.position.x - transform.position.x) * Mathf.Rad2Deg + 45f;
         Quaternion targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
-        turretRotationPoint.rotation = targetRotation;
+        turretRotationPoint.rotation = Quaternion.RotateTowards(turretRotationPoint.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 
     private bool CheckTargetInRange()
