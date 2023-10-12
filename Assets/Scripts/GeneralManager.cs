885ca994fb36af0cffecf449f7837925b317be90
Babi Soraggi <108243062+bpsoraggi@users.noreply.github.com>
2023-10-12 15:08:28 -0300

    UI components (#53)

---
+++ Assets/Scripts/GeneralManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GeneralManager : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+    public void ChangeScene(string scene)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(scene);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
