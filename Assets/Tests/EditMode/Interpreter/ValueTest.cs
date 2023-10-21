4a71f361d996c87ed7228cf6519ff6531dee5734
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2023-10-20 23:56:35 -0300

    Add nil value type

--- Assets/Tests/EditMode/Interpreter/ValueTest.cs
+++ Assets/Tests/EditMode/Interpreter/ValueTest.cs
@@ -21,13 +21,13 @@ namespace Tests.EditMode.Interpreter
         public void TestCast()
         {
             var n = Value.WrapNumber(3.14);
-            Assert.AreEqual(n.AsNumber, 3.14);
-            var ex1 = Assert.Throws<EvalException>(() => { _ = n.AsString; });
+            Assert.AreEqual(n.AsNumber(), 3.14);
+            var ex1 = Assert.Throws<EvalException>(() => { _ = n.AsString(); });
             Assert.AreEqual(ex1.Message, "Expected type String, but got Number");
 
             var s = Value.WrapString("oi");
-            Assert.AreEqual(s.AsString, "oi");
-            var ex2 = Assert.Throws<EvalException>(() => { _ = s.AsNumber; });
+            Assert.AreEqual(s.AsString(), "oi");
+            var ex2 = Assert.Throws<EvalException>(() => { _ = s.AsNumber(); });
             Assert.AreEqual(ex2.Message, "Expected type Number, but got String");
         }
 
