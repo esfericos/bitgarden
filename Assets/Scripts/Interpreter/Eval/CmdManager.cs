cb4060511a7437484836a0924b329bc4cfd543db
Luiz Felipe Gonçalves <git@luizfelipe.dev>
2023-10-20 23:36:54 -0300

    Type check commands

--- Assets/Scripts/Interpreter/Eval/CmdManager.cs
+++ Assets/Scripts/Interpreter/Eval/CmdManager.cs
@@ -19,53 +19,68 @@ namespace Interpreter.Eval
         public readonly IReadOnlyDictionary<string, Type> ArgTypes;
         public readonly Type ReturnType;
 
-        private object CmdInstance;
-        private MethodInfo CmdExec;
+        private readonly object _cmdInstance;
+        private readonly MethodInfo _cmdExec;
         // We must keep track of argument order to not rely on a specific dictionary impl.
-        private readonly IReadOnlyList<string> ImplArgNames;
+        private readonly IReadOnlyList<string> _implArgNames;
 
         private const string MustBeValue = "must derive from 'Value' (i.e., be a 'String', 'Number' or 'Nil')";
 
         public RegisteredCmd(object cmdInstance)
         {
-            Name = "TODO";
-
-            CmdInstance = cmdInstance;
+            _cmdInstance = cmdInstance;
             var cmd = cmdInstance.GetType();
+
+            // Function name is the same as the class, but the first character is lowercased (camelCase).
+            Name = char.ToLower(cmd.Name[0]) + cmd.Name[1..];
             
             // Tries to get the `Exec` method
-            CmdExec = cmd.GetMethod("Exec", BindingFlags.Public | BindingFlags.Instance);
-            if (CmdExec == null)
+            _cmdExec = cmd.GetMethod("Exec", BindingFlags.Public | BindingFlags.Instance);
+            if (_cmdExec == null)
                 throw new Exception($"Missing 'Exec' method in command class '{cmd.Name}'");
             
             // Initialize ArgTypes
-            var execParams = CmdExec.GetParameters();
+            var execParams = _cmdExec.GetParameters();
             var argTypes = new Dictionary<string, Type>();
             var argNames = new List<string>();
             foreach (var param in execParams)
             {
                 if (!Value.IsValueType(param.ParameterType))
-                    throw new Exception($"Param '{param.Name}' of command class '{cmd.Name}' {MustBeValue}");
+                    throw new Exception($"Argument '{param.Name}' of command class '{cmd.Name}' {MustBeValue}");
                 argTypes.Add(param.Name, param.ParameterType);
                 argNames.Add(param.Name);
             }
             ArgTypes = argTypes;
-            ImplArgNames = argNames;
+            _implArgNames = argNames;
             
             // Return type
-            if (!Value.IsValueType(CmdExec.ReturnType))
+            if (!Value.IsValueType(_cmdExec.ReturnType))
                 throw new Exception($"Return type of 'Exec' from command class '{cmd.Name}' {MustBeValue}");
-            ReturnType = CmdExec.ReturnType;
+            ReturnType = _cmdExec.ReturnType;
         }
 
         public Value Exec(IReadOnlyDictionary<string, Value> args)
         {
-            // TODO: Type check arguments.
-            var argValues = ImplArgNames
-                .Select(argName => (object)args[argName])
+            var argValues = _implArgNames
+                .Select(argName =>
+                {
+                    if (!args.TryGetValue(argName, out var value))
+                        throw new EvalException($"Missing argument '{argName}' in call to '{Name}'");
+                    var actualTy = value.GetType();
+                    var expectedTy = ArgTypes[argName];
+                    // Checks whether type `actualTy` is assignable to `expectedTy`
+                    if (!expectedTy.IsAssignableFrom(actualTy))
+                        throw new EvalException($"Type mismatch in argument '{argName}' in call to '{Name}': "
+                            + $"expected type '{expectedTy.Name}', but got type '{actualTy.Name}'");
+                    return (object)value;
+                })
                 .ToArray();
+            // Ensure the user didn't pass any unnecessary arguments.
+            // (one's using foreach so that it doesn't execute if there are no extra arguments)
+            foreach (var invalidArg in args.Keys.Except(_implArgNames))
+                throw new EvalException($"Argument '{invalidArg}' is not needed in call to '{Name}'");
             // This cast is infallible since the constructor ensures that `CmdExec`'s return type is a value type.
-            return (Value)CmdExec.Invoke(CmdInstance, argValues);
+            return (Value)_cmdExec.Invoke(_cmdInstance, argValues);
         }
     }
 }
--- Assets/Tests/EditMode/Interpreter/CmdManagerTest.cs
+++ Assets/Tests/EditMode/Interpreter/CmdManagerTest.cs
@@ -1,30 +1,151 @@
+using System;
 using System.Collections.Generic;
 using Interpreter.Eval;
 using NUnit.Framework;
+using String = Interpreter.Eval.String;
 
 namespace Tests.EditMode.Interpreter
 {
     public class CmdManagerTest
     {
-        [Test]
-        public void TestBasicExec()
+        [TestCaseSource(nameof(SuccessCasesData))]
+        public void SuccessCases((object, string, Dictionary<string, Value>, Value) tup)
         {
-            var c = new RegisteredCmd(new TestCmd());
-            var retValue = c.Exec(new Dictionary<string, Value>()
-            {
-                { "x", Value.WrapString("hello") },
-                { "y", Value.WrapNumber(3.14) },
-                { "z", Value.WrapString("bye") },
-            });
-            Assert.AreEqual("hello 3.14 String(\"bye\")", retValue.AsString);
+            var (cmd, expectedCmdName, userArgs, expectedReturnValue) = tup;
+            var c = new RegisteredCmd(cmd);
+            Assert.AreEqual(expectedCmdName, c.Name);
+            var returnValue = c.Exec(userArgs);
+            Assert.AreEqual(expectedReturnValue, returnValue);
+        }
+
+        [TestCaseSource(nameof(ConstructorErrorCasesData))]
+        public void ConstructorErrorCases((object, Dictionary<string, Value>, string) tup)
+        {
+            var (cmd, _, expectedErrorMessage) = tup;
+            var ex = Assert.Throws<Exception>(() => _ = new RegisteredCmd(cmd));
+            Assert.That(ex.Message, Does.StartWith(expectedErrorMessage));
+        }
+        
+        [TestCaseSource(nameof(ExecErrorCasesData))]
+        public void ExecErrorCases((object, Dictionary<string, Value>, string) tup)
+        {
+            var (cmd, userArgs, expectedErrorMessage) = tup;
+            var c = new RegisteredCmd(cmd);
+            var ex = Assert.Throws<EvalException>(() => _ = c.Exec(userArgs));
+            Assert.That(ex.Message, Does.StartWith(expectedErrorMessage));
+        }
+
+        public static IEnumerable<(object, string, Dictionary<string, Value>, Value)> SuccessCasesData()
+        {
+            yield return (
+                    new TestCmd.BasicCmd(),
+                    "basicCmd",
+                    new Dictionary<string, Value>()
+                    {
+                        { "x", Value.WrapString("hello") },
+                        { "y", Value.WrapNumber(3.14) },
+                        { "z", Value.WrapString("bye") },
+                    },
+                    Value.WrapString("hello 3.14 String(\"bye\")"));
+            yield return (
+                new TestCmd.CmdWithoutArguments(),
+                "cmdWithoutArguments",
+                new Dictionary<string, Value>(),
+                Value.WrapNumber(3.14));
+        }
+
+        public static IEnumerable<(object, Dictionary<string, Value>, string)> ConstructorErrorCasesData()
+        {
+            var emptyDict = new Dictionary<string, Value>();
+            
+            yield return (
+                new TestCmd.WithoutExec(),
+                emptyDict,
+                "Missing 'Exec' method in command class 'WithoutExec'");
+            yield return (
+                new TestCmd.WithNonValueReturnType(),
+                emptyDict,
+                "Return type of 'Exec' from command class 'WithNonValueReturnType' must derive from 'Value'");
+            yield return (
+                new TestCmd.WithNonValueArgumentType(),
+                emptyDict,
+                "Argument 'invalidArg' of command class 'WithNonValueArgumentType' must derive from 'Value'");
+        }
+        
+        public static IEnumerable<(object, Dictionary<string, Value>, string)> ExecErrorCasesData()
+        {
+            yield return (
+                new TestCmd.BasicCmd(),
+                new Dictionary<string, Value>()
+                {
+                    { "x", Value.WrapString("hello") },
+                    // missing y
+                    { "z", Value.WrapString("bye") },
+                },
+                "Missing argument 'y' in call to 'basicCmd'");
+            yield return (
+                new TestCmd.BasicCmd(),
+                new Dictionary<string, Value>()
+                {
+                    { "x", Value.WrapString("hello") },
+                    { "y", Value.WrapString("!!!wrong!!!") }, // <-- should be number
+                    { "z", Value.WrapString("bye") },
+                },
+                "Type mismatch in argument 'y' in call to 'basicCmd': expected type 'Number', but got type 'String'");
+            yield return (
+                new TestCmd.BasicCmd(),
+                new Dictionary<string, Value>()
+                {
+                    { "x", Value.WrapString("hello") },
+                    { "y", Value.WrapNumber(3.14) },
+                    { "z", Value.WrapString("bye") },
+                    { "extraArg", Value.WrapString("extra") }, // <-- extra argument
+                },
+                "Argument 'extraArg' is not needed in call to 'basicCmd'");
         }
     }
 
-    internal class TestCmd
+    namespace TestCmd
     {
-        public String Exec(String x, Number y, Value z)
+        internal class BasicCmd
         {
-            return new String($"{x.Value} {y.Value} {z}");
+            public String Exec(String x, Number y, Value z)
+            {
+                return new String($"{x.Value} {y.Value} {z}");
+            }
+        }
+
+        internal class CmdWithoutArguments
+        {
+            public Number Exec()
+            {
+                return new Number(3.14);
+            }
+        }
+
+        internal class WithoutExec
+        {
+            public String Foo(String x)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        internal class WithNonValueReturnType
+        {
+            public void Exec(String _)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        internal class WithNonValueArgumentType
+        {
+            public String Exec(bool invalidArg)
+            {
+                _ = invalidArg;
+                throw new NotImplementedException();
+            }
         }
     }
 }
