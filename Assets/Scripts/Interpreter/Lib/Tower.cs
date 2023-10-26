101fe5b607d791852b2359ad53f8165d25011595
Eduardo Lemos <103197804+lemosep@users.noreply.github.com>
2023-10-26 06:47:10 -0300

    Created tower builder interpreter script (#73)

---
+++ Assets/Scripts/Interpreter/Lib/Tower.cs
@@ -0,0 +1,23 @@
+﻿using Interpreter.Eval.Type;
+using UnityEngine;
+
+namespace Interpreter.Lib.Tower
+{
+    public class CreateTower : MonoBehaviour
+    {
+        public GameObject obj;
+
+        public Nil Exec(Number x, Number y)
+        {
+            //@TODO: check for resources (waiting @ClaraOMello)
+
+            float xPosition = (float) x.AsNumber();
+            float yPosition = (float)y.AsNumber();
+
+            GameObject tower = Instantiate(obj);
+            tower.transform.position = new Vector3(xPosition, yPosition);
+
+            return new Nil();
+        }
+    }
+}
