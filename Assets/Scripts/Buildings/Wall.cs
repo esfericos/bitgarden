0459b8a2d1d8b81997801b279b173bbb76dcf5f7
Eduardo Lemos <dudulemox@gmail.com>
2023-10-26 09:52:51 -0300

    tower health added

---
+++ Assets/Scripts/Buildings/Wall.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class Wall : Entity
+{
+    [Header("References")]
+    [SerializeField] private HealthBarBehaviour HealthBar;
+    
+    [Header("Attributes")]
+    [SerializeField] private int Hitpoints;
+    [SerializeField] private int MaxHitpoints;
+    
+    public override Price Price { get; set; }
+    public override void Start()
+    {
+        Hitpoints = MaxHitpoints;
+        HealthBar.SetHealth(Hitpoints, MaxHitpoints);   
+    }
+    
+    public void TakeHit(int damage)
+    {   
+        Price = new Price(gold: 1);
+        Hitpoints -= damage;
+        HealthBar.SetHealth(Hitpoints, MaxHitpoints);
+
+        if (Hitpoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
