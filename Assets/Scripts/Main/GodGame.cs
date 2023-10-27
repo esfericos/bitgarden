44078cae18bec839ff3634467930ff65f5501a6b
Babi Soraggi <108243062+bpsoraggi@users.noreply.github.com>
2023-10-27 17:18:25 -0300

    Script de criação de muros funcional (#91)

--- Assets/Scripts/Main/GodGame.cs
+++ Assets/Scripts/Main/GodGame.cs
@@ -27,7 +27,7 @@ namespace Main
                 .AddCmd(new Lib.Math.Sub())
                 .AddCmd(new Lib.Math.Mul())
                 .AddCmd(new Lib.Math.Div())
-                .AddCmd(new Lib.Wall.CreateWall(GetComponent<GraphManager>()))
+                .AddCmd(new Lib.Wall.CreateWall())
                 .AddCmd(new Lib.Tower.CreateTower());
         }
     }
