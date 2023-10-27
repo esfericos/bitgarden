ee474e90be28842d3813147e60e1cb1f5cba9744
Yuri <49888082+YuriRDev@users.noreply.github.com>
2023-10-26 21:43:19 -0300

    InterpreterCall (#80)

---
+++ Assets/Scripts/Console.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Main;
+using Interpreter.Eval;
+
+public class Console : MonoBehaviour
+{
+
+    public TMP_InputField commandInput;
+    public TMP_Text response;
+    public Button submitButton;
+    public GodGame GodGame;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GodGame = GameObject.FindGameObjectWithTag("GodGame").GetComponent<GodGame>();
+
+        submitButton.onClick.AddListener(OnButtonClick);
+    }
+
+    private void OnButtonClick()
+    {
+        if (commandInput.text == "") return;
+        Debug.Log("[CMD] Received: >: " + commandInput.text);
+
+        string interpreterResponse = "Ok!";
+        try
+        {
+            GodGame.Evaluator.ExecProgram(commandInput.text);
+        }
+        catch (EvalException e)
+        {
+            interpreterResponse = e.Message;
+        }
+
+        // Call Code interpreter here inside a try/catch call... If catch print:
+        response.text = "> " + commandInput.text + " \n" + interpreterResponse + "\n\n" + response.text;
+
+
+        // clear command input
+        commandInput.text = "";
+
+
+    }
+}
