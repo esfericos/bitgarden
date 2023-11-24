d3d48a284e9fef69a95bd5ca619451fe4b0207e9
Eduardo Lemos <dudulemox@gmail.com>
2023-11-24 09:18:46 -0300

    finished core ✅

--- Assets/Scripts/Buildings/Core.cs
+++ Assets/Scripts/Buildings/Core.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using Interfaces;
 using UnityEngine;
-
-public class Core : Entity
-{
+public class Core : Entity, IDamageable
+{   
+    [Header("References")]
+    [SerializeField] private HealthBarBehaviour HealthBar;
+    
+    [Header("Attribute")]
+    [SerializeField] private int Hitpoints;
+    [SerializeField]private int MaxHitpoints = 30;
     public static bool IsDead { set; get; }
     public override Price Price { get; set; }
 
@@ -12,8 +17,22 @@ public class Core : Entity
     {
         Price = new Price();
         IsDead = false;
+        Hitpoints = MaxHitpoints;
+        HealthBar.SetHealth(Hitpoints, MaxHitpoints);
     }
 
+    public void TakeDamage(int damage)
+    {
+        Hitpoints -= damage;
+        HealthBar.SetHealth(Hitpoints, MaxHitpoints);
+
+        if (Hitpoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    
     // No onDestroy, ou metodo semelhante,
     // nao esquecer de colocar IsDead = true
 }
