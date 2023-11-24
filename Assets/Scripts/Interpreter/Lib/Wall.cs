bb588bb9b6b357ceddaccc8486d0fc71dd872652
Babi Soraggi <108243062+bpsoraggi@users.noreply.github.com>
2023-11-24 07:15:22 -0300

    Loja funcional (#106)

--- Assets/Scripts/Interpreter/Lib/Wall.cs
+++ Assets/Scripts/Interpreter/Lib/Wall.cs
@@ -3,91 +3,95 @@ using System.Collections.Generic;
 using Interpreter.Eval;
 using Interpreter.Eval.Type;
 using Main;
+using TMPro;
 using UnityEngine;
 
 namespace Interpreter.Lib.Wall
 {
     public class CreateWall : MonoBehaviour
     {
+        private Store store = GameObject.FindGameObjectWithTag("StoreHandler").GetComponent<Store>();
+        [SerializeField] private Price price = new Price(5);
         public Nil Exec(Number x, Number y)
         {
-            //@TODO: check for resources
-            
-            Graph grafo = GameObject.FindGameObjectWithTag("GraphHandle").GetComponent<Graph>();
-            if (!grafo.IsAvailableToBuild(new Position((ushort)x.Value, (ushort)y.Value)))
+            if (!store.Buy(price))
             {
-                throw new EvalException("Nao pode seu retardado");
-            }
-
-            int type = 0;
-            Sprite[] sprites = Resources.LoadAll<Sprite>("Sprites/wall_basic");
-            
-            ushort xf = (ushort)x.AsNumber();
-            ushort yf = (ushort)y.AsNumber();
-            
-            Collider2D[] plot = Physics2D.OverlapAreaAll(new Vector2(xf + 0.05f, yf + 0.03f), new Vector2(xf + 0.05f, yf + 0.03f));
-
-            if (plot.Length > 0)
-            {
-                Console.print("nao pode");
-                //@TODO: terminal warning
+                //TODO terminal warning
+                Console.print("ta pobre hein");
             }
             else
             {
-                Collider2D[] surroundings = Physics2D.OverlapAreaAll(new Vector2(xf - 0.95f, yf - 0.9f), new Vector2(xf + 1.05f, yf + 1.1f));
+                int type = 0;
+                Sprite[] sprites = Resources.LoadAll<Sprite>("Sprites/wall_basic");
                 
-                foreach (var obj in surroundings)
-                {
-                    int objType = obj.GetComponent<global::Wall>().Type;
-                    int otherType = 0;
+                ushort xf = (ushort)x.AsNumber();
+                ushort yf = (ushort)y.AsNumber();
+                
+                Collider2D[] plot = Physics2D.OverlapAreaAll(new Vector2(xf + 0.05f, yf + 0.03f), new Vector2(xf + 0.05f, yf + 0.03f));
 
-                    // get rid of corners
-                    if ((obj.transform.position.x > (float)(xf + 0.05) &&
-                         obj.transform.position.y > (float)(yf + 0.1)) ||
-                        (obj.transform.position.x > (float)(xf + 0.05) &&
-                         obj.transform.position.y < (float)(yf + 0.1)) ||
-                        (obj.transform.position.x < (float)(xf + 0.05) &&
-                         obj.transform.position.y > (float)(yf + 0.1)) ||
-                        (obj.transform.position.x < (float)(xf + 0.05) && obj.transform.position.y < (float)(yf + 0.1)))
-                    {
-                        continue; }
-                    
-                    if (obj.transform.position.x > (float)(xf + 0.05)) { type += 2; otherType = 8; }
-                    if (obj.transform.position.x < (float)(xf + 0.05)) { type += 8; otherType = 2; }
-                    if (obj.transform.position.y > (float)(yf + 0.1)) { type += 1; otherType = 4; }
-                    if (obj.transform.position.y < (float)(yf + 0.1)) { type += 4; otherType = 1; }
-                    
-                    objType += otherType;
-                    obj.GetComponent<global::Wall>().Type = objType;
-                    
-                    if (objType == 1) { obj.GetComponent<SpriteRenderer>().sprite = sprites[7]; }
-                    if (objType == 2 || objType == 3) { obj.GetComponent<SpriteRenderer>().sprite = sprites[0]; }
-                    if (objType == 4) { obj.GetComponent<SpriteRenderer>().sprite = sprites[10]; }
-                    if (objType == 5) { obj.GetComponent<SpriteRenderer>().sprite = sprites[9]; }
-                    if (objType == 6 || objType == 7) { obj.GetComponent<SpriteRenderer>().sprite = sprites[2]; }
-                    if (objType == 8 || objType == 9) { obj.GetComponent<SpriteRenderer>().sprite = sprites[1]; }
-                    if (objType == 10) { obj.GetComponent<SpriteRenderer>().sprite = sprites[4]; }
-                    if (objType == 11) { obj.GetComponent<SpriteRenderer>().sprite = sprites[5]; }
-                    if (objType == 12 || objType == 13) { obj.GetComponent<SpriteRenderer>().sprite = sprites[3]; }
-                    if (objType == 14 || objType == 15) { obj.GetComponent<SpriteRenderer>().sprite = sprites[6]; }
+                if (plot.Length > 0)
+                {
+                    Console.print("nao pode");
+                    //@TODO: terminal warning
                 }
-                
-                GameObject wall = Instantiate(Resources.Load("Prefabs/Buildings/Wall"), new Vector3(xf + 0.05f, yf + 0.1f, 1), Quaternion.identity) as GameObject;
-                
-                if (type == 0 || type == 1) { wall.GetComponent<SpriteRenderer>().sprite = sprites[7]; }
-                if (type == 2 || type == 3) { wall.GetComponent<SpriteRenderer>().sprite = sprites[0]; }
-                if (type == 4) { wall.GetComponent<SpriteRenderer>().sprite = sprites[10]; }
-                if (type == 5) { wall.GetComponent<SpriteRenderer>().sprite = sprites[9]; }
-                if (type == 6 || type == 7) { wall.GetComponent<SpriteRenderer>().sprite = sprites[2]; }
-                if (type == 8 || type == 9) { wall.GetComponent<SpriteRenderer>().sprite = sprites[1]; }
-                if (type == 10) { wall.GetComponent<SpriteRenderer>().sprite = sprites[4]; }
-                if (type == 11) { wall.GetComponent<SpriteRenderer>().sprite = sprites[5]; }
-                if (type == 12 || type == 13) { wall.GetComponent<SpriteRenderer>().sprite = sprites[3]; }
-                if (type == 14 || type == 15) { wall.GetComponent<SpriteRenderer>().sprite = sprites[6]; }
-                
-                wall.GetComponent<global::Wall>().Type = type;
+                else
+                {
+                    
+                    
+                    Collider2D[] surroundings = Physics2D.OverlapAreaAll(new Vector2(xf - 0.95f, yf - 0.9f), new Vector2(xf + 1.05f, yf + 1.1f));
+                    
+                    foreach (var obj in surroundings)
+                    {
+                        int objType = obj.GetComponent<global::Wall>().Type;
+                        int otherType = 0;
+
+                        // get rid of corners
+                        if ((obj.transform.position.x > (float)(xf + 0.05) &&
+                             obj.transform.position.y > (float)(yf + 0.1)) ||
+                            (obj.transform.position.x > (float)(xf + 0.05) &&
+                             obj.transform.position.y < (float)(yf + 0.1)) ||
+                            (obj.transform.position.x < (float)(xf + 0.05) &&
+                             obj.transform.position.y > (float)(yf + 0.1)) ||
+                            (obj.transform.position.x < (float)(xf + 0.05) && obj.transform.position.y < (float)(yf + 0.1)))
+                        {
+                            continue; }
+                        
+                        if (obj.transform.position.x > (float)(xf + 0.05)) { type += 2; otherType = 8; }
+                        if (obj.transform.position.x < (float)(xf + 0.05)) { type += 8; otherType = 2; }
+                        if (obj.transform.position.y > (float)(yf + 0.1)) { type += 1; otherType = 4; }
+                        if (obj.transform.position.y < (float)(yf + 0.1)) { type += 4; otherType = 1; }
+                        
+                        objType += otherType;
+                        obj.GetComponent<global::Wall>().Type = objType;
+                        
+                        if (objType == 1) { obj.GetComponent<SpriteRenderer>().sprite = sprites[7]; }
+                        if (objType == 2 || objType == 3) { obj.GetComponent<SpriteRenderer>().sprite = sprites[0]; }
+                        if (objType == 4) { obj.GetComponent<SpriteRenderer>().sprite = sprites[10]; }
+                        if (objType == 5) { obj.GetComponent<SpriteRenderer>().sprite = sprites[9]; }
+                        if (objType == 6 || objType == 7) { obj.GetComponent<SpriteRenderer>().sprite = sprites[2]; }
+                        if (objType == 8 || objType == 9) { obj.GetComponent<SpriteRenderer>().sprite = sprites[1]; }
+                        if (objType == 10) { obj.GetComponent<SpriteRenderer>().sprite = sprites[4]; }
+                        if (objType == 11) { obj.GetComponent<SpriteRenderer>().sprite = sprites[5]; }
+                        if (objType == 12 || objType == 13) { obj.GetComponent<SpriteRenderer>().sprite = sprites[3]; }
+                        if (objType == 14 || objType == 15) { obj.GetComponent<SpriteRenderer>().sprite = sprites[6]; }
+                    }
+                    
+                    GameObject wall = Instantiate(Resources.Load("Prefabs/Buildings/Wall"), new Vector3(xf + 0.05f, yf + 0.1f, 1), Quaternion.identity) as GameObject;
+                    
+                    if (type == 0 || type == 1) { wall.GetComponent<SpriteRenderer>().sprite = sprites[7]; }
+                    if (type == 2 || type == 3) { wall.GetComponent<SpriteRenderer>().sprite = sprites[0]; }
+                    if (type == 4) { wall.GetComponent<SpriteRenderer>().sprite = sprites[10]; }
+                    if (type == 5) { wall.GetComponent<SpriteRenderer>().sprite = sprites[9]; }
+                    if (type == 6 || type == 7) { wall.GetComponent<SpriteRenderer>().sprite = sprites[2]; }
+                    if (type == 8 || type == 9) { wall.GetComponent<SpriteRenderer>().sprite = sprites[1]; }
+                    if (type == 10) { wall.GetComponent<SpriteRenderer>().sprite = sprites[4]; }
+                    if (type == 11) { wall.GetComponent<SpriteRenderer>().sprite = sprites[5]; }
+                    if (type == 12 || type == 13) { wall.GetComponent<SpriteRenderer>().sprite = sprites[3]; }
+                    if (type == 14 || type == 15) { wall.GetComponent<SpriteRenderer>().sprite = sprites[6]; }
+                    
+                    wall.GetComponent<global::Wall>().Type = type;
+                }
             }
-            
             return new Nil();
         }
     }
